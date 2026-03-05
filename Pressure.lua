d662d3b2d00b759de6cc1813f13f14a74afdeb5a
kinamy200111 <vladik.dvornikov@inbox.ru>
2026-03-05 22:29:58 +0300

    Update Pressure.lua

--- Pressure.lua
+++ Pressure.lua
@@ -5,7 +5,7 @@ print("Version 5.0")
 print("Most changes with UI, Tab settings")
 print("Fix bugs with no esp for paper password")
 print("Small changes for watermark")
-print("==SOON Support doors==")
+print("!!!!BROKEN ALL ESP, BECAUSE ESP LIBRARY IS DISCOUNTED!!!!")
 
 local Mouse = game.Players.LocalPlayer:GetMouse()
 
